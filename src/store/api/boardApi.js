99d5bcd99f450597c9aea78dabe7fe9dfed2ba53
yerin-158 <hanblueblue@gmail.com>
2020-06-07 20:17:20 +0900

    일반적인 api 호출함수로 변경

--- src/store/api/boardApi.js
+++ src/store/api/boardApi.js
@@ -1,14 +1,6 @@
 import axios from 'axios';
 import { DOMAIN } from '../../static/constant';
 
-export async function get(pageNumber, pageSize) {
-    try {
-        return await axios.get(DOMAIN+'/api/boards?page='+pageNumber+'&size='+pageSize);
-    } catch (error) {
-        const response = { data : {
-                code : error.response.status,
-                message: error.message
-            }};
-        return response;
-    }
+export function get(pageNumber, pageSize) {
+    return axios.get(DOMAIN+'/api/boards?page='+pageNumber+'&size='+pageSize);
 }
