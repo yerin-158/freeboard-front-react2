48acd55c9af3d18e7511b9bba876ec815c4a1d7b
yerin-158 <hanblueblue@gmail.com>
2020-06-07 22:38:45 +0900

    헬퍼를 이용한 데이터 가공추가

--- src/store/modules/board/action.js
+++ src/store/modules/board/action.js
@@ -1,16 +1,12 @@
 import type from './type'
 import {get} from "../../api/boardApi";
+import {getData} from "../../../helper/boardHelper";
 
 export const changePage = (pageNumber, pageSize) => dispatch => {
-    var testData = [{name: 'Mehmet', surname: '안녕하세요 ^^ 5-1', birthYear: 1987, birthCity: 63},
-        {name: 'Zerya Betül', surname: '글 제목입니다.', birthYear: 2017, birthCity: 34},
-        {name: 'Bread', surname: '게시판 테스트', birthYear: 2011, birthCity: 34},
-        {name: 'Jonny', surname: 'Material-table 이용하기', birthYear: 2012, birthCity: 17},
-        {name: 'Sera', surname: '페이징 처리 이용하기', birthYear: 2007, birthCity: 17}];
 
-    return get(pageNumber, pageSize)
+    return get(pageNumber+1, pageSize)
         .then(response => {
-            const selectedData = testData.concat(response.data.contents).concat(testData);
+            const selectedData = getData(pageNumber, pageSize, response);
             dispatch({
                 type: type.CHANGE_PAGE,
                 payload: {
