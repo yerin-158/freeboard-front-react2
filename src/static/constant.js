88209d8cedf1ff010670305e165d9ebd5a92ebf2
yerin-158 <hanblueblue@gmail.com>
2020-06-12 21:11:26 +0900

    검색 타입 상수 추가

--- src/static/constant.js
+++ src/static/constant.js
@@ -1,2 +1,8 @@
-export const DOMAIN = "/freeboard01"
+export const DOMAIN = "/freeboard01";
 export const BOARD_PAGE_SIZE = 5;
+export const searchType = [
+    {type: "ALL", print: "제목+내용"},
+    {type: "CONTENTS", print: "내용"},
+    {type: "TITLE", print: "제목"},
+    {type: "WRITER", print: "작성자"}
+]
