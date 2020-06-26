d35f635b8fd82fcee297714a28358997daa5e80a
robin.son <robin.son@kakaocorp.com>
2020-06-26 23:18:52 +0900

    댓글 입력란 추가

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import {Button, TextField} from "@material-ui/core";
+import EditIcon from '@material-ui/icons/Edit';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,9 +19,28 @@ const useStyles = makeStyles((theme) => ({
 export default function Comments({comments, userLoggedIn}) {
     const classes = useStyles();
 
+    var newComment = "";
+    const setNewComment = (value) => {
+        newComment = value;
+    }
+
+
+
 
     return (
         <div className={classes.root}>
+            <Grid container spacing={1} alignItems="flex-end">
+                <Grid item xs={11}>
+                    <TextField id="input-with-icon-grid"
+                               fullWidth
+                               placeholder={typeof userLoggedIn !== 'undefined' ? "댓글을 입력하세요." : "로그인 해주세요."}
+                               onChange={event => setNewComment(event.target.value)}/>
+                </Grid>
+                <Grid item xs>
+                    <EditIcon color="primary" size="medium"
+                            onClick={() => alert(newComment)} />
+                </Grid>
+            </Grid>
             {comments.map((comment, index) => {
                 return (
                     <Grid container spacing={1}>
