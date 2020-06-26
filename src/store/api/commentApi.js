f6b81139494ad96a523a85a7604b1cafa83a29c6
robin.son <robin.son@kakaocorp.com>
2020-06-26 23:47:02 +0900

    새로운 댓글 추가 요청 함수 추가

--- src/store/api/commentApi.js
+++ src/store/api/commentApi.js
@@ -5,3 +5,6 @@ export function getComments(boardId){
     return axios.get(DOMAIN+'/api/comments?boardId='+boardId);
 }
 
+export function addComment(contents, boardId){
+    return axios.post(DOMAIN+'/api/comments?boardId='+boardId, {contents: contents});
+}
