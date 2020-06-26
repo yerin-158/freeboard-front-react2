56e2bba228ae22d37799f2e3b35137a124844048
robin.son <robin.son@kakaocorp.com>
2020-06-26 23:48:46 +0900

    댓글 입력 아이콘 클릭 시 서버에 post 요청을 전송하고, 성공하면 rowClick 액션 생성 함수를 재호출한다.

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import {Button, TextField} from "@material-ui/core";
+import {TextField} from "@material-ui/core";
 import EditIcon from '@material-ui/icons/Edit';
+import {addComment} from "../store/api/commentApi";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,16 +16,13 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function Comments({comments, userLoggedIn}) {
+export default function Comments({comments, userLoggedIn, boardId, handleAddComment}) {
     const classes = useStyles();
 
     var newComment = "";
     const setNewComment = (value) => {
         newComment = value;
     }
-
-
-
 
     return (
         <div className={classes.root}>
@@ -38,24 +35,35 @@ export default function Comments({comments, userLoggedIn}) {
                 </Grid>
                 <Grid item xs>
                     <EditIcon color="primary" size="medium"
-                            onClick={() => alert(newComment)} />
+                              onClick={() => {
+                                  addComment(newComment, boardId)
+                                      .then(response => {
+                                          if (typeof response.data.code != "undefined") {
+                                              alert(response.data.message);
+                                          } else {
+                                              const queryParam = response.config.url.split('?');
+                                              handleAddComment({id: queryParam[1].replace(/[^0-9]/g, '')});
+                                          }
+                                      });
+                              }}/>
                 </Grid>
             </Grid>
             {comments.map((comment, index) => {
-                return (
-                    <Grid container spacing={1}>
-                        <Grid item xs>
-                            {comment.writer.accountId}
+                    return (
+                        <Grid container spacing={1}>
+                            <Grid item xs>
+                                {comment.writer.accountId}
+                            </Grid>
+                            <Grid item xs={6}>
+                                {comment.contents}
+                            </Grid>
+                            <Grid item xs>
+                                {comment.createdAt.monthValue + '/' + comment.createdAt.dayOfMonth + ' (' + comment.createdAt.hour + ':' + comment.createdAt.minute + ')'}
+                                {comment.writer.accountId === userLoggedIn ? ' X' : null}
+                            </Grid>
                         </Grid>
-                        <Grid item xs={6}>
-                            {comment.contents}
-                        </Grid>
-                        <Grid item xs>
-                            {comment.createdAt.monthValue + '/' + comment.createdAt.dayOfMonth + ' (' + comment.createdAt.hour + ':' + comment.createdAt.minute + ')'}
-                            {comment.writer.accountId === userLoggedIn ? ' X' : null }
-                        </Grid>
-                    </Grid>
-                )}
+                    )
+                }
             )}
         </div>
     );
